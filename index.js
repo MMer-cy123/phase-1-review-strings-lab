f27b85679edb91b4ab5f75ed6b84fb2b26e66f51
Mercy <mercy.mwongeli@student.moringaschool.com>
2023-06-21 18:06:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Mercy Mwongeli';
+const welcomeMessage = 'Welcome to Flatbook, + ${Mercy Mwongeli}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+currentUser.slice(0, 1);
+const shortGreeting = 'Welcome, ${M.slice(0, 1)}!';
